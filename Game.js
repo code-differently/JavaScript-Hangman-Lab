d310f0fe40ae9f65e699706433fb99cdbdf64b64
Amatullah Brown <amatullahbrown@Amatullahs-MBP.fios-router.home>
2021-01-22 23:10:49 -0500

    user can enter word, still struggling with process.stdin.on and repeating game

--- Game.js
+++ Game.js
@@ -1,44 +1,41 @@
 const readline = require('readline');
 const Hangman = require('./Hangman');
+const prompt = require('prompt-sync')();
 
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
-
-// let hangman = new Hangman('Code Differently');
-let hangman = new Hangman('Add');
-
+const word = prompt('Please enter word to guess: ');
+const hangman = new Hangman(word); 
+    
 console.log(`Remaining Guesses: ${hangman.getRemainingGuesses()}`);
 console.log(hangman.getDisplayMessage());
 console.log("Next Guess:");
 
 process.stdin.on('keypress', (str, key) => {
-
-    if(key.sequence === '\u0003'){
+    // console.log("hangman at process.stdin", hangman); //at this point, inappropriate instance of the hangman object when trying to allow for second round of play
+    if (key.sequence === '\u0003') {
         process.exit();
     }
-    else{
-
+    else {
         hangman.guessLetter(str);
         console.log(`${str}\n`);
-
-        if(hangman.isMessageSolved()){
-            console.log(`You won! The hidden message was ${hangman.getWord()}`);
+        
+        if (hangman.isMessageSolved()) {
+            console.log(`You won! The hidden message was ${hangman.word}`); //second time around, display message and word are accurate but you won is logged after first guess
             process.exit();
         }
 
-        else if(hangman.getRemainingGuesses() > 0){
+        else if (hangman.getRemainingGuesses() > 0) {
             console.log(`Remaining Guesses: ${hangman.getRemainingGuesses()}`);
             console.log(hangman.getDisplayMessage());
             console.log("Next Guess:");
         }
 
-        else{
-            console.log(`Game Over! The hidden message was ${hangman.getWord()}`);
+        else {
+            console.log(`Game Over! The hidden message was ${hangman.word}`); //same issue
             process.exit();
         }
     }
 
 })
-
-
