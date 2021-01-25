8f04ac2188ee6415faf191f21767ad42d20d7311
Stephanie Czetli <stephanieczetli@gmail.com>
2021-01-25 00:32:24 -0500

    first commit - 5 of 7 pass

--- Hangman.js
+++ Hangman.js
@@ -1,17 +1,43 @@
 
 class Hangman {
+    constructor(phrase) { 
+        this.phrase = phrase;
+        this.guesses = 6;
+        this.message = [];
+        this.lettersGuess = [];
+       
+    }
 
     getDisplayMessage(){
-        return null;
+        const regex = /\S/g;
+        let displayMessage = this.phrase.replace(regex, '*');
+        let position = this.phrase.search(this.letter);
+
+        return displayMessage;
     }
 
-    guessLetter(letter){}
-
-    getRemainingGuesses(){
-        return -1;
+    
+    guessLetter(letter) {
+        var count = 0; //number of times we find the letter in our message
+        for (var i = 0; i < this.phrase.length; i++) {
+            if ( letter.toLowerCase() == this.phrase[i].toLowerCase() ) {
+                count++;
+                this.lettersGuess += letter; //stores correctly guessed letter
+            }
+        }
+        if (count == 0) {
+            this.guesses --;
+        }
     }
 
-    isMessageSolved(){
+
+    getRemainingGuesses() {
+
+        return this.guesses;
+    }
+
+    isMessageSolved() {
+        
         return false;
     }
 
