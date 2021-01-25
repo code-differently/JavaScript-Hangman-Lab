ef825b5f31ad15fc4caa4676e5f812582c305b0a
Kim Lai <kimlai@Kims-MacBook-Air.local>
2021-01-25 10:33:47 -0500

    fix display

--- Hangman.js
+++ Hangman.js
@@ -1,22 +1,23 @@
 
 class Hangman {
     constructor (word){
+        
        this.word = word;
        this.counter = 6;
        this.guessedLetter = [];
     }
     getDisplayMessage(){
 
-        const wordSeperatedBySpace = this.word.split(" ");
-        return wordSeperatedBySpace
-              .map((wordInPhrase) => {
-                return wordInPhrase
-                  .split("")
-                  .map((letter) => { return this.guessedLetter.includes(letter.toLowerCase()) ? letter : "*";})
-                  .join("");
-              })
-              .join(" ");
-           
+            const wordSeperatedBySpace = this.word.split(" ");
+            return this.getRemainingGuesses() ?  wordSeperatedBySpace
+                .map((wordInPhrase) => {
+                    return wordInPhrase
+                    .split("")
+                    .map((letter) => { return this.guessedLetter.includes(letter.toLowerCase()) ? letter : "*";})
+                    .join("");
+                })
+                .join(" ") : this.word;
+          
     }
 
     guessLetter(letter){
