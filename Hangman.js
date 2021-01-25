887d9929ea5369e26a198741554f71449566a71c
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-01-24 22:36:50 -0500

    changed program completely

--- Hangman.js
+++ Hangman.js
@@ -1,41 +1,45 @@
-
 class Hangman {
-    constructor(phrase) {
-        this.phrase = phrase;
-    }// end constructor
-    getDisplayMessage() {
-        this.phrase = this.phrase.split('');
-        let message = [];
-        let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z','A','B','C','D','E','F','G','H',
-    'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-        for (let i = 0; i < this.phrase.length; i++) {
-            for (let j = 0; j < alphabet.length; j++){
-             if (this.phrase[i] === alphabet[j]) {
-                 message[i] = '*';
-                 j = alphabet.length;
-             } else {
-                 message[i] = ' ';
-             }
-          }//end j loop 
-        }//end i loop
-        return message.join('');
-    } //end getDisplayMessage
 
-    guessLetter(letter) {
-        
-    }//end guessLetter
+    constructor(phrase){
+        this.phrase = phrase;
+        this.guesses = 6;
+        this.message = [];
+        this.lettersGuess = [];
+        this.letter = [];
+    }
+
+    getDisplayMessage(){
+        const regex = /\S/g;
+        let displayMessage = this.phrase.replace(regex, '*');
+        let position = this.phrase.search(this.letter);
+            console.log(position);
+
+        return displayMessage;
+    }
+
+    guessLetter(letter){  
+        var count = 0;
+        this.letter = letter;
+        for (var i = 0; i < this.phrase.length; i++) {
+                if(letter.toLowerCase() == this.phrase[i].toLowerCase()) {
+                    count ++;
+                    this.lettersGuess += letter;
+                } 
+            }
+            if (count == 0){
+                this.guesses --;
+            }
+    }
 
     getRemainingGuesses(){
-        return -1;
-    }//end getRemainingGuesses
+        
+        return this.guesses;
+    }
 
     isMessageSolved(){
         return false;
-    }//end isMessageSolved
+    }
 
-}//end class
+}
 
 module.exports = Hangman;
-
