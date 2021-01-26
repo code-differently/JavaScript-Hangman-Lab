c6eb32e63154855266fd561e5ab71472623af7d6
Donna Murphy <dmurf@Donnas-Air.lan>
2021-01-25 21:59:03 -0500

    finished

--- Hangman.js
+++ Hangman.js
@@ -1,77 +1,31 @@
 
 class Hangman {
-    constructor(phrase) {
-        this.originalPhrase = phrase;
-        this.guess = 'Code Differently';
-        // create another variable using the this keyword for the current number of guesses, starting with 6 guesses
-    }
-
-    getDisplayMessage(){
-        // return the current guess (i.e. ***e *****e**)
-        return this.guess;
-    }
-
-    guessLetter(letter){
-        // does that letter exist in the original phrase? 
-            // if it does, add letter to guess
-            // if it does not, substract one from the number of guesses
-    }
-
-    getRemainingGuesses(){
-        // returning the current number of guesses
-        return -1;
-    }
-
-    isMessageSolved(){
-        // return whether or not the user has guessed all the letters in the phrase
-        return false;
-    }
-
-}
-
-
-// class Student {
-//     constructor(name) {
-//         this.name = name;
-//     }
-// }
-
-// const dominique = new Student('dominique');
-// const shannon = new Student("Shannon");
-
-// console.log(dominique.name) // dominique
-// console.log(shannon.name) // shannon
-
-
-
-module.exports = Hangman;
-
-class Hangman {
-
     constructor(phrase){
         this.phrase = phrase;
         this.guesses = 6;
-        this.message = [];
-        this.lettersGuess = [];
-        this.letter = [];
+        this.letterGuess = [];
     }
 
     getDisplayMessage(){
-        const regex = /\S/g;
-        let displayMessage = this.phrase.replace(regex, '*');
-        let position = this.phrase.search(this.letter);
-            console.log(position);
+    const splitWords = this.phrase.split(" ");
+    return this.getRemainingGuesses() ?  splitWords
+        .map((wordInPhrase) => {
+            return wordInPhrase
+            .split("")
+            .map((letter) => { return this.letterGuess.includes(letter.toLowerCase()) ? letter : "*";})
+            .join("");
+        })
+        .join(" ") : this.phrase;
+      
+}
 
-        return displayMessage;
-    }
-
-    guessLetter(letter){  
+guessLetter(letter){  
         var count = 0;
         this.letter = letter;
         for (var i = 0; i < this.phrase.length; i++) {
                 if(letter.toLowerCase() == this.phrase[i].toLowerCase()) {
                     count ++;
-                    this.lettersGuess += letter;
+                    this.letterGuess += letter;
                 } 
             }
             if (count == 0){
@@ -80,14 +34,15 @@ class Hangman {
     }
 
     getRemainingGuesses(){
-        
         return this.guesses;
     }
 
     isMessageSolved(){
-        return false;
+        const currentMessage = this.getDisplayMessage().split('');
+        return currentMessage.every(letter => {return letter == "*" ? false:true});
     }
 
 }
 
 module.exports = Hangman;
+
